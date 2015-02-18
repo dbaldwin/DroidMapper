f53577d006857f70d6003e0f7e1141a426ff9c82
Dennis Baldwin <dennisbaldwin@gmail.com>
2015-02-18 08:00:16 -0600

    Adding option to trigger camera on distance

--- app/src/main/java/com/droidmapper/MainActivity.java
+++ app/src/main/java/com/droidmapper/MainActivity.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -25,12 +27,14 @@ public class MainActivity extends ActionBarActivity {
 
     // Constants used as keys for the values saved in preferences:
     private static final String PREF_KEY_DB_OAUTH2_ACCESS_TOKEN = "PREF_KEY_DB_OAUTH2_ACCESS_TOKEN";
-    private static final String PREF_KEY_INTERVAL = "PREF_KEY_INTERVAL";
+    private static final String PREF_KEY_INTERVAL_DISTANCE = "PREF_KEY_INTERVAL_DISTANCE";
+    private static final String PREF_KEY_INTERVAL_TIME = "PREF_KEY_INTERVAL_TIME";
     private static final String PREF_KEY_DELAY = "PREF_KEY_DELAY";
     private static final String PREF_KEY_SIZE = "PREF_KEY_SIZE";
 
     // Views:
-    private Spinner spinnerInterval, spinnerDelay, spinnerSize;
+    private Spinner spinnerIntervalTime, spinnerIntervalDistance, spinnerDelay, spinnerSize;
+    private RadioButton radioButtonTime, radioButtonDistance;
     private Button buttonStart;
 
     // Other:
@@ -53,25 +57,38 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         // Get references to views defined in the GUI:
-        spinnerInterval = (Spinner) findViewById(R.id.activityMain_spinnerInterval);
+        spinnerIntervalDistance = (Spinner) findViewById(R.id.activityMain_spinnerIntervalDistance);
+        spinnerIntervalTime = (Spinner) findViewById(R.id.activityMain_spinnerIntervalTime);
         spinnerDelay = (Spinner) findViewById(R.id.activityMain_spinnerDelay);
         spinnerSize = (Spinner) findViewById(R.id.activityMain_spinnerSize);
         buttonStart = (Button) findViewById(R.id.activityMain_buttonStart);
+        radioButtonDistance = (RadioButton) findViewById(R.id.activityMain_radioButtonDistance);
+        radioButtonTime = (RadioButton) findViewById(R.id.activityMain_radioButtonTime);
 
         // Listen for spinner selected item changes:
-        spinnerInterval.setOnItemSelectedListener(onItemSelectedListener);
+        spinnerIntervalDistance.setOnItemSelectedListener(onItemSelectedListener);
+        spinnerIntervalTime.setOnItemSelectedListener(onItemSelectedListener);
         spinnerDelay.setOnItemSelectedListener(onItemSelectedListener);
         spinnerSize.setOnItemSelectedListener(onItemSelectedListener);
+
+        // Disable the distance spinner:
+        spinnerIntervalDistance.setEnabled(false);
 
         // Listen for clicks on the start button:
         buttonStart.setOnClickListener(onClickListener);
 
+        // Listen for clicks on the radio buttons:
+        radioButtonDistance.setOnCheckedChangeListener(onCheckedChangeListener);
+        radioButtonTime.setOnCheckedChangeListener(onCheckedChangeListener);
+
         // Load the previously selected values from preferences and set them in GUI:
         sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        int prefIntervalPos = sharedPrefs.getInt(PREF_KEY_INTERVAL, 0);
+        int prefIntervalDistancePos = sharedPrefs.getInt(PREF_KEY_INTERVAL_DISTANCE, 0);
+        int prefIntervalTimePos = sharedPrefs.getInt(PREF_KEY_INTERVAL_TIME, 0);
         int prefDelayPos = sharedPrefs.getInt(PREF_KEY_DELAY, 0);
         int prefSizePos = sharedPrefs.getInt(PREF_KEY_SIZE, 0);
-        spinnerInterval.setSelection(prefIntervalPos);
+        spinnerIntervalDistance.setSelection(prefIntervalDistancePos);
+        spinnerIntervalTime.setSelection(prefIntervalTimePos);
         spinnerDelay.setSelection(prefDelayPos);
         spinnerSize.setSelection(prefSizePos);
 
@@ -126,8 +143,10 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
             // Save the newly selected value in preferences:
-            if (parent == spinnerInterval) {
-                sharedPrefs.edit().putInt(PREF_KEY_INTERVAL, position).commit();
+            if (parent == spinnerIntervalDistance) {
+                sharedPrefs.edit().putInt(PREF_KEY_INTERVAL_DISTANCE, position).commit();
+            } else if (parent == spinnerIntervalTime) {
+                sharedPrefs.edit().putInt(PREF_KEY_INTERVAL_TIME, position).commit();
             } else if (parent == spinnerDelay) {
                 sharedPrefs.edit().putInt(PREF_KEY_DELAY, position).commit();
             } else if (parent == spinnerSize) {
@@ -169,19 +188,60 @@ public class MainActivity extends ActionBarActivity {
                     // Load the selected values:
                     Resources res = getResources();
                     String authTkn = sharedPrefs.getString(PREF_KEY_DB_OAUTH2_ACCESS_TOKEN, null);
-                    int interval = res.getIntArray(R.array.photo_interval_values)[spinnerInterval.getSelectedItemPosition()];
+                    int intervalDistance = res.getIntArray(R.array.photo_interval_distance_values)[spinnerIntervalDistance.getSelectedItemPosition()];
+                    int intervalTime = res.getIntArray(R.array.photo_interval_time_values)[spinnerIntervalTime.getSelectedItemPosition()];
                     int delay = res.getIntArray(R.array.photo_delay_values)[spinnerDelay.getSelectedItemPosition()];
                     float size = Float.parseFloat(res.getStringArray(R.array.photo_size_values)[spinnerSize.getSelectedItemPosition()]);
 
                     // Launch the CameraActivity:
                     Intent intent = new Intent(MainActivity.this, CameraActivity.class);
                     intent.putExtra(CameraActivity.EXTRA_DB_OAUTH2_ACCESS_TOKEN, authTkn);
-                    intent.putExtra(CameraActivity.EXTRA_INTERVAL, interval);
+                    if (radioButtonDistance.isChecked()) {
+                        intent.putExtra(CameraActivity.EXTRA_INTERVAL_TYPE, CameraActivity.INTERVAL_TYPE_DISTANCE);
+                        intent.putExtra(CameraActivity.EXTRA_INTERVAL, intervalDistance);
+                    } else {
+                        intent.putExtra(CameraActivity.EXTRA_INTERVAL_TYPE, CameraActivity.INTERVAL_TYPE_TIME);
+                        intent.putExtra(CameraActivity.EXTRA_INTERVAL, intervalTime);
+                    }
                     intent.putExtra(CameraActivity.EXTRA_DELAY, delay);
                     intent.putExtra(CameraActivity.EXTRA_SIZE, size);
                     startActivity(intent);
                 }
             }
+        }
+    };
+
+    private RadioButton.OnCheckedChangeListener onCheckedChangeListener = new RadioButton.OnCheckedChangeListener() {
+
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            radioButtonDistance.setOnCheckedChangeListener(null);
+            radioButtonTime.setOnCheckedChangeListener(null);
+
+            if (buttonView == radioButtonDistance) {
+                if (isChecked) {
+                    radioButtonTime.setChecked(false);
+                    spinnerIntervalDistance.setEnabled(true);
+                    spinnerIntervalTime.setEnabled(false);
+                } else {
+                    radioButtonTime.setChecked(true);
+                    spinnerIntervalDistance.setEnabled(false);
+                    spinnerIntervalTime.setEnabled(true);
+                }
+            } else if (buttonView == radioButtonTime) {
+                if (isChecked) {
+                    radioButtonDistance.setChecked(false);
+                    spinnerIntervalDistance.setEnabled(false);
+                    spinnerIntervalTime.setEnabled(true);
+                } else {
+                    radioButtonDistance.setChecked(true);
+                    spinnerIntervalDistance.setEnabled(true);
+                    spinnerIntervalTime.setEnabled(false);
+                }
+            }
+
+            radioButtonDistance.setOnCheckedChangeListener(onCheckedChangeListener);
+            radioButtonTime.setOnCheckedChangeListener(onCheckedChangeListener);
         }
     };
 }
